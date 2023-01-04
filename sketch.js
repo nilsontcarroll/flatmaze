6c29324628d19b3667e792583675bf3fa52d871e
nilsontcarroll <93231858+nilsontcarroll@users.noreply.github.com>
2023-01-03 20:54:32 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,15 +2,16 @@
 //code=MIT license. artistic work = cc 3.0
 //modified by nilson 2023
 
+//first number is X, second number is Y. 0,0 is in top left corner
 let itemText = [
-  ["Everything here seemed really very bad with no way out.",0,20],
-  ["graveyard-bodied\nself-unfulfilled these burial-plot scabs.\ni wish i could hug my 12 year old self.",520,500],
+  ["Everything here seemed really very bad with no way out.\nI apologize to myself in 12th grade,\nand to my best friend feeling bad in the locker room\nand to my dad.",30,20],
+  ["graveyard-bodied\nself-unfulfilled these burial-plot scabs deepen.\ni wish i could hug my 12 year old self\nand play secret of mana with them.",600,840],
   ["I stood at the threshold of the maze of my mistakes.",1800,1500],
   ["I'm sorry.",2200,2000],
   ["There were two paths and I was sure I would pick the bad one. \nSo I picked nothing.",1050,200],
-  ["in the distance i heard the plants\nfor the first time in a long time",220,1850],
+  ["In the distance i heard the plants\nfor the first time in a long time:\ntheir language was subtle.",220,1850],
   ["i spoke with the mummies for the second time that night...\n'this boy is a graveyard' they said.\njust tell me the answer to the riddle i cried tears all over my stupid tshirt.",2300,450],
-  ["Face practically burned right off,\nmy reflection grumbled sweet nothings\nstone-wall-boy who lost some friends.",1400,620] 
+  ["Face practically burned right off,\nmy reflection grumbled sweet nothings\ndungeon-brick-boy who lost some friends.",1400,620] 
     //no comma after last one!
 ];
 
@@ -46,12 +47,14 @@ function setup() {
   textSize(24);
 
   //create a sprite and add the 3 animations
+    
+  //pass start location to createSprite
   player = createSprite(300, 300);
 
   var myAnimation = player.addAnimation('moving', 'assets/player/player1.png', 'assets/player/player2.png', 'assets/player/player3.png');
   myAnimation.frameDelay = 8; //slow down the animation
 
-  //create some background for visual reference
+  //load your background image
   frame = loadImage('assets/background.png');
 
 }
